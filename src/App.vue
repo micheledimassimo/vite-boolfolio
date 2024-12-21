41df74a19bcb639ee555003484d37874dea39557
micheledimassimo <mcldms@outlook.it>
2024-12-22 00:04:33 +0100

    pulizia codice

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <script>
-/* 
-  Per importare ed utilizzare un componente dentro un altro devo SEMPRE seguire questi 3 passi:
-  1) Importazione del componente
-  2) Dichiarazione del componente
-  3) Utilizzo del componente
-*/
-// 1) Importazione del componente
+
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 
@@ -14,7 +8,7 @@ export default {
     return { 
     }
   },
-  // 2) Dichiarazione del componente
+
   components: {
     AppHeader,
     AppMain,
@@ -27,7 +21,7 @@ export default {
 
 <template>
   <div>
-    <!-- 3) Utilizzo del componente -->
+
     <AppHeader />
 
     <AppMain />
